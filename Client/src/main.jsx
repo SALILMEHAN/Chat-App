8840c52f55500aa484e85aac9660721e88118e7c
SALIL <salilmehan123@gmail.com>
2024-07-07 12:00:05 +0530

    c1

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = "https://chat-app-backend-1zyb.onrender.com"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
